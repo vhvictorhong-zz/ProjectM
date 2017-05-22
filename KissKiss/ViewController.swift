fdfafbbb70a823b3d45c0f311af1997e6a511b60
victorhong1988 <victorhong1988@gmail.com>
2017-05-22 14:06:57 -0400

    Initial Commit

---
+++ KissKiss/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KissKiss
+//
+//  Created by Victor Hong on 22/05/2017.
+//  Copyright © 2017 Victor Hong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
